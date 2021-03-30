5a81b30eef1156d702cf76b2b0199e9ddaa7b4ed
Cheng Tang <cheng21tang@yahoo.com>
2021-03-30 16:32:10 -0700

    added function showQuestions and updateQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-// ACCEPTANCE CRITERIA
+window.addEventListener("load", function() {
+    // ACCEPTANCE CRITERIA
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
@@ -22,20 +23,34 @@
         // I need to then show the first question
         // I need to create the questions and the correct answers
         // That first question needs to have 4 answer selections the users can choose
-// If the user chooses the correct answer then I need to go to the next question
+// When the user choose any answer it needs to go to the next question
+// I need to be able to keep track of the users score; it should start at 'zero'
+// If the user chooses the correct answer I need to add +10 to their score
 // If the user chooses the incorrect answer then I need to deduct 10 seconds off of the timer and then go to the next question
 // the previous 3 steps need to repeat until the timer reaches 0
 // When the timer reaches zero I want to display "Game Over" and show their "score"
-// the score will be +10 points for each questions answered correctly
-// There will not be a negative score for unanswered questions only a deduction of -10 seconds off of the timer
 // When the score is displayed I need to allow the user to enter their initials
-// I need to display the "Play Again" button again (this will be the same as the "Start Game" button)
-// I need a "View Highscores" button on the screen where the user can click and see the highscores
+// I need to store their score and initials on the "high-score.html" page locally
 // When i refresh the page the highscores stay stored locally
 // if i close the webpage and reopen the high scores will be blank
+// I need to display the "Play Again" button again (this will be the same as the "Start Quiz" button and will restart the loop for the game)
+        // I need a "View Highscores" button on the screen where the user can click and see the highscores
+
 
 var start = document.getElementById("startButton")
 start.addEventListener("click", startQuiz);
+
+var currentQuestion = 0;
+var answerButtons = document.getElementsByClassName("answerBtn")
+answerButtons.addEventListener("click", function() {
+    currentQuestion++
+    updateQuestion(currentQuestion);
+});
+
+function currentQuestion() {
+    currentQuestion++
+    updateQuestion(currentQuestion);
+}
 
 // var hide = document.getElementById("startButton")
 // start.addEventListener("click", hideStartButton);
@@ -70,17 +85,31 @@ function hideStartButton() {
 // Shows questions
 function showQuestions() {
     var x = document.getElementById("quiz");
-    if (x.style.display === "none") {
         x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+}
+
+function updateQuestion(bananapancakes) {
+    var question = document.getElementById("question")
+    var answer1 = document.getElementById("answer-1")
+    var answer2 = document.getElementById("answer-2")
+    var answer3 = document.getElementById("answer-3")
+    var answer4 = document.getElementById("answer-4")
+    question.textContent = codeQuestions[bananapancakes].question
+    answer1.textContent = codeQuestions[bananapancakes].answers[0]
+    answer2.textContent = codeQuestions[bananapancakes].answers[1]
+    answer3.textContent = codeQuestions[bananapancakes].answers[2]
+    answer4.textContent = codeQuestions[bananapancakes].answers[3]
+
+
 }
 
 function startQuiz() {
     countdown();
     hideStartButton();
     showQuestions();
+    updateQuestion(currentQuestion);
+    var answerButtons = document.getElementsByClassName("answerBtn")
+    console.log(answerButtons);
 }
 
 
@@ -162,3 +191,5 @@ var codeQuestions = [
         answers: [':root', '*root', '*', '.root']
     }
 ]
+})
+
